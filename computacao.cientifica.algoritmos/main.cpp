2400e1584db94c032ae1471b558369b3ab048af2
Rafael Salgado <thomassalgado@gmail.com>
2017-06-07 19:29:41 -0300

    Initial Commit

---
+++ computacao.cientifica.algoritmos/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  computacao.cientifica.algoritmos
+//
+//  Created by Rafael Thomas Salgado on 07/06/17.
+//  Copyright © 2017 Rafael Thomas Salgado. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
